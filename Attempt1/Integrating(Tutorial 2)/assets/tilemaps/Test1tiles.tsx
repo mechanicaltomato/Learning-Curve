a77a4c64b80c188b00c39fb90267683b65b56c44
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2019-08-16 18:35:19 -0300

    Tutorial de como integrar o phaser com o Tiled

---
+++ Attempt1/Integrating(Tutorial 2)/assets/tilemaps/Test1tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="32" tileheight="32" tilecount="880" columns="22">
+ <image source="../images/tiles.png" width="705" height="1280"/>
+</tileset>
