89529ede6b80ba5bfad914faf52adbb02fd4579c
RFPadilha <ricardofpad@hotmail.com>
2019-08-16 16:49:09 -0300

    Misturando Tiled com Phaser, ainda seguindo tutoriais.

--- Attempt1/SpaceHipster(Tutorial1)/main.js
+++ Attempt1/SpaceHipster(Tutorial1)/main.js
@@ -3,9 +3,11 @@ var SpaceHipster = SpaceHipster || {};
 SpaceHipster.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
  
 SpaceHipster.game.state.add('Boot', SpaceHipster.Boot);
-//uncomment these as we create them through the tutorial
-//SpaceHipster.game.state.add('Preload', SpaceHipster.Preload);
-//SpaceHipster.game.state.add('MainMenu', SpaceHipster.MainMenu);
-//SpaceHipster.game.state.add('Game', SpaceHipster.Game);
+uncomment these as we create them through the tutorial
+SpaceHipster.game.state.add('Preload', SpaceHipster.Preload);
+SpaceHipster.game.state.add('MainMenu', SpaceHipster.MainMenu);
+SpaceHipster.game.state.add('Game', SpaceHipster.Game);
  
 SpaceHipster.game.state.start('Boot');
+
+//following the tutorial on https://gamedevacademy.org/html5-phaser-tutorial-spacehipster-a-space-exploration-game/
